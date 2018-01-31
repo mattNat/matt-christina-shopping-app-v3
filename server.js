cc3858a20ddf15157b4de3d8a0c6f7f803640bf3
mattNat <mattsanpedro76@gmail.com>
2018-01-31 09:17:10 -0800

    finish it

--- server.js
+++ server.js
@@ -53,6 +53,11 @@ app.delete('/shopping-list/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.delete('/recipes/:id', (req, res) => {req
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe list item \`${req.params.id}\``);
+  res.status(204).end();
+})
 
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
@@ -68,7 +73,7 @@ app.post('/recipes', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = Recipes.create(req.body.name, req.body.ingredients);
+  const item = Recipes.create(req.bodyfor.name, req.body.ingredients);
   res.status(201).json(item);
 });
 
